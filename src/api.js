ef3eb37e1f72d41ae37e9d6caf9873fdf8bcfa4b
Patrick O'Connor <pattyo20@optonline.net>
2022-12-15 18:30:07 -0500

    completed setting up the search component

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fbc03abb26msh153f31b30a12a86p1d95f3jsndfcd45f7f4fc',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
