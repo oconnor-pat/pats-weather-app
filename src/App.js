a0b3f6a3529903e28290c284fc140fecc98331ef
Patrick O'Connor <pattyo20@optonline.net>
2023-01-08 17:17:33 -0500

    added a dark mode theme toggle button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import ToggleDarkTheme from "./components/themetoggle/themetoggle";
 import Search from "./components/search/search";
 import Forecast from "./components/forecast/forecast";
 import CurrentWeather from "./components/current-weather/current-weather";
@@ -38,6 +39,7 @@ function App() {
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
       {forecast && <Forecast data={forecast} />}
+      <ToggleDarkTheme>ToggleDarkTheme</ToggleDarkTheme>
     </div>
   );
 }
